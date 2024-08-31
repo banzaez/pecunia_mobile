3a4b63dfa6c51221beb140736c76a176566c68f9
Pavel Fedorov <banzaez@gmail.com>
2024-08-31 22:09:26 +0300

    swipe delete

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -17,27 +17,25 @@ class TransactionItem extends GetWidget<TransactionController> {
         onDismissed: (direction) => controller.deleteSQL(transaction.id),
         confirmDismiss: _confirmDismiss,
         background: Container(color: Colors.red),
-        child: Card(
-            color: Colors.white10,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white10,
-                child: transaction.amount > 0
-                    ? const Icon(Icons.attach_money, color: Colors.green)
-                    : const Icon(Icons.money_off, color: Colors.red),
-              ),
-              title: Text.rich(TextSpan(
-                children: [
-                  TextSpan(
-                      text:
-                          "${transaction.amount > 0 ? "transaction_item_income".tr : "transaction_item_expense".tr}: ",
-                      style: AppTextStyle.text12w400()),
-                  TextSpan(text: " ${transaction.category}", style: AppTextStyle.text14w400()),
-                ],
-              )),
-              subtitle: Text(transaction.amount.formatSum, style: AppTextStyle.text16w400()),
-              trailing: _date(),
-            )),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.white10,
+            child: transaction.amount > 0
+                ? const Icon(Icons.attach_money, color: Colors.green)
+                : const Icon(Icons.money_off, color: Colors.red),
+          ),
+          title: Text.rich(TextSpan(
+            children: [
+              TextSpan(
+                  text:
+                      "${transaction.amount > 0 ? "transaction_item_income".tr : "transaction_item_expense".tr}: ",
+                  style: AppTextStyle.text12w400()),
+              TextSpan(text: " ${transaction.category}", style: AppTextStyle.text14w400()),
+            ],
+          )),
+          subtitle: Text(transaction.amount.formatSum, style: AppTextStyle.text16w400()),
+          trailing: _date(),
+        ),
       );
 
   // --------------------------------------------------------------------------------------------
