90abf2b6fb456951fea6f3162cbdd6901373ddeb
Pavel Fedorov <banzaez@gmail.com>
2024-09-12 15:39:30 +0300

    details

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -3,6 +3,4 @@ class AppConstants {
 
   static const isProdServer = true;
   static const supportEmail = "banzaez@gmail.com";
-
-  static const clientId = "207680397216-v0455lksm4mkum5d4acibpaae9v3mr5b.apps.googleusercontent.com";
 }
